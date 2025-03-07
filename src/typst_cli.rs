a2c2b8d77153a8d3751633ab3f59fd369a264493
kokic <kokicplus@gmail.com>
2025-03-07 15:01:46 +0800

    release: v0.2.5 - étoile et toi (#4)
    feat: typst export html (experimental)
    
    feat: use marco `file!` & `line!` instead ad-hoc string
    
    feat: custom reference taxon
    
    chore: upgrade version to `v0.2.5`

--- src/typst_cli.rs
+++ src/typst_cli.rs
@@ -5,42 +5,46 @@ use crate::{
     html, html_flake,
 };
 
-pub fn write_svg(typst_path: &str, svg_path: &str) -> Result<(), std::io::Error> {
-    if !verify_and_file_hash(typst_path)? && Path::new(svg_path).exists() {
+pub fn write_html(typst_path: &str, html_path: &str) -> Result<(), std::io::Error> {
+    if !verify_and_file_hash(typst_path)? && Path::new(html_path).exists() {
         println!("Skip: {}", crate::slug::pretty_path(Path::new(typst_path)));
         return Ok(());
     }
 
     let root_dir = config::root_dir();
     let full_path = config::join_path(&root_dir, typst_path);
-    let output = Command::new("typst")
-        .arg("c")
-        .arg("-f=svg")
-        .arg(format!("--root={}", root_dir))
-        .arg(full_path)
-        .arg("-")
-        .stdout(std::process::Stdio::piped())
-        .output()?;
+    let content = fs::read_to_string(full_path).expect(concat!(file!(), '#', line!()));
+    let content = export_html_config(&content);
 
-    if output.status.success() {
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        let thematized = thematize(stdout);
-        fs::write(svg_path, thematized)?;
-        
-        println!(
-            "Compiled to SVG: {}",
-            crate::slug::pretty_path(Path::new(svg_path))
-        );
-    } else {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Command failed in `write_svg`: \n  {}", stderr);
-    }
+    let html = source_to_html(&content, &root_dir)?;
+    fs::write(html_path, html)?;
+    println!(
+        "Compiled to HTML: {}",
+        crate::slug::pretty_path(Path::new(html_path))
+    );
+
     Ok(())
 }
 
-fn thematize(s: std::borrow::Cow<'_, str>) -> String {
-    let index = s.rfind("</svg>").unwrap();
-    format!("{}<style>\n{}\n</style>\n</svg>", &s[0..index], html_flake::html_typst_style())
+fn export_html_config(content: &str) -> String {
+    format!(
+        r#"
+#show: html.elem.with("html")
+#let elem-frame(content, attrs: (:), tag: "div") = html.elem(tag, html.frame(content), attrs: attrs)
+#show math.equation.where(block: true): elem-frame.with(tag: "span", attrs: ("style": "display: flex; justify-content: center; overflow: auto;"))
+#show math.equation.where(block: false): elem-frame.with(tag: "span", attrs: ("style": "display: inline;"))
+    {}
+    "#,
+        content
+    )
+}
+
+pub fn read_typst_html_body(html_path: &str) -> Result<String, std::io::Error> {
+    let html = fs::read_to_string(html_path)?;
+    let start_pos = html.find("<html>").expect(concat!(file!(), '#', line!())) + 6;
+    let end_pos = html.rfind("</html>").expect(concat!(file!(), '#', line!()));
+    let content = &html[start_pos..end_pos];
+    return Ok(content.to_string());
 }
 
 pub struct InlineConfig {
@@ -81,36 +85,92 @@ pub fn source_to_inline_svg(src: &str, config: InlineConfig) -> Result<String, s
     ))
 }
 
+pub fn source_to_html(src: &str, root_dir: &str) -> Result<String, std::io::Error> {
+    compile_source(src, root_dir, "html", Some("--features=html"))
+}
+
 pub fn source_to_svg(src: &str, root_dir: &str) -> Result<String, std::io::Error> {
-    let child = if cfg!(target_os = "windows") {
-        Command::new("powershell")
-            .arg("/C")
-            .arg(format!(
-                "echo '{}' | typst c -f=svg --root={} - -",
-                src, root_dir
-            ))
-            .stdout(std::process::Stdio::piped())
-            .spawn()
-            .expect("Failed to spawn shell process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg(format!(
-                "echo '{}' | typst c -f=svg --root={} - -",
-                src, root_dir
-            ))
-            .stdout(std::process::Stdio::piped())
-            .spawn()
-            .expect("Failed to spawn shell process")
-    };
-    let output = child.wait_with_output().expect("Failed to read stdout");
+    compile_source(src, root_dir, "svg", None)
+}
+
+pub fn compile_source(
+    src: &str,
+    root_dir: &str,
+    output_format: &str,
+    extra: Option<&str>,
+) -> Result<String, std::io::Error> {
+    let buffer_path = config::buffer_path();
+    fs::write(&buffer_path, src)?;
+
+    let extra_and_buffer = extra.map_or(vec![buffer_path.to_string()], |s| {
+        vec![s.to_string(), buffer_path.to_string()]
+    });
+    let output = Command::new("typst")
+        .arg("c")
+        .arg(format!("-f={}", output_format))
+        .arg(format!("--root={}", root_dir))
+        .args(extra_and_buffer)
+        .arg("-")
+        .stdout(std::process::Stdio::piped())
+        .output()?;
+    fs::remove_file(buffer_path)?;
 
     Ok(if output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
         stdout.to_string()
     } else {
         let stderr = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Command failed with error:\n{}", stderr);
+        eprintln!(
+            "Command failed in {}: \n  {}",
+            concat!(file!(), '#', line!()),
+            stderr
+        );
         String::new()
     })
+}
+
+pub fn write_svg(typst_path: &str, svg_path: &str) -> Result<(), std::io::Error> {
+    if !verify_and_file_hash(typst_path)? && Path::new(svg_path).exists() {
+        println!("Skip: {}", crate::slug::pretty_path(Path::new(typst_path)));
+        return Ok(());
+    }
+
+    let root_dir = config::root_dir();
+    let full_path = config::join_path(&root_dir, typst_path);
+    let output = Command::new("typst")
+        .arg("c")
+        .arg("-f=svg")
+        .arg(format!("--root={}", root_dir))
+        .arg(full_path)
+        .arg("-")
+        .stdout(std::process::Stdio::piped())
+        .output()?;
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let thematized = thematize(stdout);
+        fs::write(svg_path, thematized)?;
+
+        println!(
+            "Compiled to SVG: {}",
+            crate::slug::pretty_path(Path::new(svg_path))
+        );
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        eprintln!(
+            "Command failed in {}: \n  {}",
+            concat!(file!(), '#', line!()),
+            stderr
+        );
+    }
+    Ok(())
+}
+
+fn thematize(s: std::borrow::Cow<'_, str>) -> String {
+    let index = s.rfind("</svg>").unwrap();
+    format!(
+        "{}<style>\n{}\n</style>\n</svg>",
+        &s[0..index],
+        html_flake::html_typst_style()
+    )
 }
