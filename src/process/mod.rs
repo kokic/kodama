48fc8b58f358ea7a5ec3ea57ab430a624cfd883b
spore <spore2050@gmail.com>
2025-07-12 22:43:35 +0800

    add ignore_paragraph as a function in `crate::process`

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -2,6 +2,8 @@
 // Released under the GPL-3.0 license as described in the file LICENSE.
 // Authors: Kokic (@kokic)
 
+use pulldown_cmark::{Event, Tag, TagEnd};
+
 pub mod content;
 pub mod embed_markdown;
 pub mod figure;
@@ -9,3 +11,15 @@ pub mod footnote;
 pub mod katex_compat;
 pub mod processer;
 pub mod typst_image;
+
+pub fn ignore_paragraph<'e, I>(events: I) -> impl Iterator<Item = Event<'e>>
+where
+    I: Iterator<Item = Event<'e>>,
+{
+    events.filter(|e| {
+        !matches!(
+            e,
+            Event::Start(Tag::Paragraph) | Event::End(TagEnd::Paragraph)
+        )
+    })
+}
