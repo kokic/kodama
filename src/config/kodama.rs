e18600b8dfd85cf8a3958b329a64de48650b0197
kokic <3250679033@qq.com>
2025-10-18 13:18:28 +0800

    chore: add authors info

--- src/config/kodama.rs
+++ src/config/kodama.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Kodama Project. All rights reserved.
+// Released under the GPL-3.0 license as described in the file LICENSE.
+// Authors: Kokic (@kokic)
+
 use serde::{Deserialize, Serialize};
 
 pub const DEFAULT_SOURCE_DIR: &str = "trees";
