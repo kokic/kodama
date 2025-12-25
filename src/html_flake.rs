fdc6cb2e568ccb8978c271bf835d07bb4bb513e5
kokic <3250679033@qq.com>
2025-12-26 01:33:47 +0800

    chore: footer element id

--- src/html_flake.rs
+++ src/html_flake.rs
@@ -30,11 +30,11 @@ pub fn html_article_inner(
     )
 }
 
-pub fn html_footer_section(summary: &str, content: &String) -> String {
+pub fn html_footer_section(id: &str, summary: &str, content: &String) -> String {
     let summary = format!("<header><h1>{}</h1></header>", summary);
     let inner_html = format!("{}{}", (html!(summary { (summary) })), content);
     let html_details = format!("<details open>{}</details>", inner_html);
-    html!(section class="block" { (html_details) })
+    html!(section class="block" id={id} { (html_details) })
 }
 
 pub fn html_section(
