b5794a2eb2bad97aee8b82777849dfe9cf9c3570
kokic <kokicplus@gmail.com>
2026-04-15 19:01:30 +0800

    chore: update copyright and author information in multiple files

--- src/compiler/anonymous_slug.rs
+++ src/compiler/anonymous_slug.rs
@@ -1,5 +1,6 @@
+// Copyright (c) 2025 Kodama Project. All rights reserved.
 // Released under the GPL-3.0 license as described in the file LICENSE.
-// Authors: Kodama contributors
+// Authors: Kokic (@kokic)
 
 use std::collections::{HashMap, HashSet};
 
--- src/compiler/subtree_slug.rs
+++ src/compiler/subtree_slug.rs
@@ -1,5 +1,6 @@
-﻿// Released under the GPL-3.0 license as described in the file LICENSE.
-// Authors: Kodama contributors
+﻿// Copyright (c) 2025 Kodama Project. All rights reserved.
+// Released under the GPL-3.0 license as described in the file LICENSE.
+// Authors: Kokic (@kokic)
 
 use std::collections::HashSet;
 
